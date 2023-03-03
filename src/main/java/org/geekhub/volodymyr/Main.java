d03aad66dc27e1665867fa0a4990912207b8682f
Volodymyr Kovalchuk <slengpack@gmail.com>
2023-03-03 15:00:43 +0200

    Initial commit

---
+++ src/main/java/org/geekhub/volodymyr/Main.java
@@ -0,0 +1,13 @@
+package org.geekhub.volodymyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.geekhub.volodymyr")
+public class Main {
+    public String PORT = System.getenv("PORT");
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
